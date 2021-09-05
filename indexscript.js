50d9936b004cc358a941135388f3da42825b8a8f
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-09-05 21:47:26 +0100

    Finished desktop menu

--- indexscript.js
+++ indexscript.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('.menu-toggle').addEventListener('click', toggleMenu)
+    makeLinkActive();
 })
 
 function toggleMenu(event){
@@ -12,14 +13,30 @@ function toggleMenu(event){
     // Get the ul of links
     links = target.parentNode.childNodes[3]
     if (target.classList.contains("open")){
-        target.classList.remove('open')
+        target.classList.remove('open');
         //change display of menu
-        links.classList.remove('mobile-links')
-        links.classList.add('links')
+        links.classList.remove('mobile-links');
+        links.classList.add('links');
     } else{
-        target.classList.add('open')
+        target.classList.add('open');
         // Change display of menu
-        links.classList.add('mobile-links')
-        links.classList.remove('links')
+        links.classList.add('mobile-links');
+        links.classList.remove('links');
+    }
+};
+
+function makeLinkActive(){
+    console.log(window.location.pathname);
+    // Get all the links
+    links = document.querySelectorAll('.nav-link')
+    for (let i = 0; i < links.length; i++){
+        currentLink = links[i]
+        if(window.location.pathname.includes(currentLink.getAttribute("href"))){
+            currentLink.classList.add('active-link')
+            console.log('ran')
+        }
+        console.log(links[i])
+        // if(links[i])
+        // links[i].classList.
     }
 }
