9eb8f126ed3993587ded7421e559e31997f86570
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-09-05 11:25:54 +0100

    Added mobile friendly menu

--- indexscript.js
+++ indexscript.js
@@ -4,13 +4,22 @@ document.addEventListener('DOMContentLoaded',function(){
 
 function toggleMenu(event){
     target = event.target
+    console.log(target)
     //Check to make sure div not i was selected
     if (target.classList.contains("menu-toggle") == false){
         target = target.parentNode
     }
+    // Get the ul of links
+    links = target.parentNode.childNodes[3]
     if (target.classList.contains("open")){
         target.classList.remove('open')
+        //change display of menu
+        links.classList.remove('mobile-links')
+        links.classList.add('links')
     } else{
         target.classList.add('open')
+        // Change display of menu
+        links.classList.add('mobile-links')
+        links.classList.remove('links')
     }
 }
